1c8d81279fab606af1461946a864368ea0596ac9
Omer Elgoni <omer.elgoni@gmail.com>
2023-07-03 18:17:24 +0200

    Added Country data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import usageData from './data/index'
+import images from './images/index'
+import countryData from './data/abridged_country_data.json'
 import { HorizontalBarChart } from './components/HorizontalBarChart';
 import useMobileDetect from './hooks/useMobileDetect';
 
 import { inject } from '@vercel/analytics';
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+import Select from 'react-select'
+
 inject();
 
+const totalNumberOfPlayers = Object.values(countryData).reduce((accumulator, value) => accumulator + value.numberOfPlayers, 0);
 const LeagueDropdown = ({ leagues, onSelect }) => {
   const [selectedLeague, setSelectedLeague] = useState('');
 
@@ -31,12 +39,49 @@ const LeagueDropdown = ({ leagues, onSelect }) => {
   );
 };
 
+const countryNames = Object.keys(countryData);
+const countrySelectOptions = []
+for (let i = 0; i < countryNames.length; i++) {
+  const countryName = countryNames[i]
+  countrySelectOptions.push({ value: countryName, label: countryName })
+}
+
+const getNHighestValues = (object, n) => {
+  let sortable = [];
+  for (var key in object) {
+    sortable.push([key, object[key]]);
+  }
+  sortable.sort(function (a, b) {
+    return a[1] - b[1];
+  });
+
+  return sortable.slice(-n)
+}
+
+
 function App() {
   const isMobile = useMobileDetect()
-  console.warn(isMobile)
   const [selectedLeague, setSelectedLeague] = useState('Rookie 1');
   const [cardFlipped, setCardFlipped] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState('Other');
   const leagueData = usageData[selectedLeague];
+
+  const FavouriteCharactersPanel = () => {
+    const favouriteCharacters = getNHighestValues(countryData[selectedCountry]?.character_frequency, 3)
+    const listItems = favouriteCharacters.map(character => {
+      return (
+        <div style={{display: 'flex', justifyContent: 'space-around', flexBasis:'50%', margin:'auto'}}>
+          <img src={images[character[0]]} style={{height: '3em'}}/>
+          <p style={{width: '50%', textAlign: 'left'}}>
+            <b>{character[0]}:</b>
+            {' ' + (character[1] * 100).toFixed(2) + '% '}
+          </p>
+        </div>
+      )
+    }
+    );
+    return <ul>{listItems}</ul>;
+  }
   const onSelect = (leagueName) => {
     setSelectedLeague(leagueName)
   }
@@ -47,50 +92,90 @@ function App() {
 
   return (
     <>
-      <h3>Street Fighter 6 Character Playrates</h3>
+      <Tabs>
+        <TabList>
+          <Tab>CharacterData</Tab>
+          <Tab>CountryData</Tab>
+        </TabList>
 
-      <div className={`card ${cardFlipped ? 'flipped' : ''}`} onClick={handleCardClick}>
-        {!cardFlipped && <div className="front">
-          <HorizontalBarChart
-            data={leagueData}
-            title={`Character Play Rates for ${selectedLeague}`}
-            isMobile={isMobile} />
-        </div>}
-        {cardFlipped && <div className="back">
-          <h2>Raw Data for {selectedLeague}</h2>
-          <table className='data-table'>
-            <thead>
-              <tr>
-                <th>Character Name</th>
-                <th>Count</th>
-              </tr>
-            </thead>
-            <tbody>
-              {leagueData.map((item) => (
-                <tr key={item.CharacterName}>
-                  <td>{item.CharacterName}</td>
-                  <td>{item.Count}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>}
-      </div >
+        <TabPanel>
+          <h3>Street Fighter 6 Character Playrates</h3>
 
-      <LeagueDropdown
-        leagues={Object.keys(usageData)}
-        onSelect={onSelect}>
+          <div className={`card ${cardFlipped ? 'flipped' : ''}`} onClick={handleCardClick}>
+            {!cardFlipped && <div className="front">
+              <HorizontalBarChart
+                data={leagueData}
+                title={`Character Play Rates for ${selectedLeague}`}
+                isMobile={isMobile} />
+            </div>}
+            {cardFlipped && <div className="back">
+              <h2>Raw Data for {selectedLeague}</h2>
+              <table className='data-table'>
+                <thead>
+                  <tr>
+                    <th>Character Name</th>
+                    <th>Count</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {leagueData.map((item) => (
+                    <tr key={item.CharacterName}>
+                      <td>{item.CharacterName}</td>
+                      <td>{item.Count}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>}
+          </div >
 
-      </LeagueDropdown>
+          <LeagueDropdown
+            leagues={Object.keys(usageData)}
+            onSelect={onSelect}>
 
-      <div>
-        <p>
-          Data last updated on 16/06/2023
-        </p>
-        <p>
-          P.S you can click the card to see raw numbers
-        </p>
-      </div>
+          </LeagueDropdown>
+
+          <div>
+            <p>
+              Data last updated on 16/06/2023
+            </p>
+            <p>
+              P.S you can click the card to see raw numbers
+            </p>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <Select options={countrySelectOptions}
+            className="react-select-container"
+            classNamePrefix="react-select"
+            onChange={(choice) => setSelectedCountry(choice.value)} />
+          <div className={`card ${cardFlipped ? 'flipped' : ''}`} onClick={handleCardClick}>
+            {!cardFlipped && <div className="front">
+              <div>
+                <h2 className='underline'>Stats for  {selectedCountry}</h2>
+                Players from {selectedCountry}s favourite characters are:
+                <FavouriteCharactersPanel />
+                <p >
+                  {selectedCountry} has an average LP of {countryData[selectedCountry]?.mean_league_points.toFixed(2)}
+                </p>
+                <p>
+                  Players from {selectedCountry} make up approx. {(countryData[selectedCountry]?.numberOfPlayers / totalNumberOfPlayers * 100).toFixed(2)} % of the playerbase
+                </p>
+              </div>
+            </div>}
+            {cardFlipped && <div className="back">
+              <h2>Extra stats for  {selectedCountry}</h2>
+              <p>
+                {countryData[selectedCountry]?.numberOfPlayers} players were sampled from {selectedCountry}
+              </p>
+              <p>
+                The LP score for players from {selectedCountry} had a standard deviation of {countryData[selectedCountry]?.std_league_points.toFixed(2)}
+              </p>
+            </div>}
+          </div >
+        </TabPanel>
+      </Tabs>
+
     </>
   )
 }
---
+++ src/images/index.js
@@ -0,0 +1,40 @@
+import blanka from './Blanka.jpg';
+import cammy from './Cammy.jpg';
+import chunLi from './Chun-Li.jpg';
+import deeJay from './Dee Jay.jpg';
+import dhalsim from './Dhalsim.jpg';
+import eHonda from './Edmond Honda.jpg';
+import jamie from './Jamie.jpg';
+import jp from './JP.jpg';
+import juri from './Juri.jpg';
+import ken from './Ken.jpg';
+import kimberly from './Kimberly.jpg';
+import lily from './Lily.jpg';
+import luke from './Luke.jpg';
+import manon from './Manon.jpg';
+import marisa from './Juri.jpg';
+import ryu from './Ryu.jpg';
+import zangief from './Zangief.jpg';
+
+
+const images = {
+    'Blanka': blanka,
+    'Cammy': cammy,
+    'Chun-Li': chunLi,
+    'Dee Jay': deeJay,
+    'Dhalsim': dhalsim,
+    'Edmond Honda': eHonda,
+    'Jamie': jamie,
+    'JP': jp,
+    'Juri': juri,
+    'Ken': ken,
+    'Kimberly': kimberly,
+    'Lily': lily,
+    'Luke': luke,
+    'Manon': manon,
+    'Marisa': marisa,
+    'Ryu': ryu,
+    'Zangief': zangief,
+};
+
+export default images;
